9a55b99a91faa545a4f567b70529fc53a224d289
Grzegorz Szpak <gszpak@student.uw.edu.pl>
2018-09-29 21:05:58 -0700

    Fair ReadWriteLock implementation

--- src/main/java/info/multithreading/problems/ReadWriteLock.java
+++ src/main/java/info/multithreading/problems/ReadWriteLock.java
@@ -2,35 +2,50 @@ package info.multithreading.problems;
 
 public class ReadWriteLock {
 
-    boolean isWriteLocked = false;
-    int readers = 0;
+    private int numReadersReading = 0;
+    private boolean isWriting = false;
+    private int numWritersWaiting = 0;
+    private int numReadersWaiting = 0;
+    private long lastReaderAllowedToEnterTimestamp = 0;
+    private boolean writerEntering = true;
 
     public synchronized void acquireReadLock() throws InterruptedException {
-
-        while (isWriteLocked) {
-            wait();
+        if (isWriting || numWritersWaiting > 0) {
+            long timeArrived = System.currentTimeMillis();
+            numReadersWaiting += 1;
+            while (timeArrived >= lastReaderAllowedToEnterTimestamp) {
+                wait();
+            }
+            numReadersWaiting--;
         }
-
-        readers++;
-    }
-
-    public synchronized void acquireWriteLock() throws InterruptedException {
-
-        while (isWriteLocked || readers != 0) {
-            wait();
-        }
-
-        isWriteLocked = true;
+        numReadersReading++;
     }
 
     public synchronized void releaseReadLock() {
-        readers--;
-        notify();
+        numReadersReading--;
+        writerEntering = (numReadersReading == 0);
+        if (writerEntering) {
+            notifyAll();
+        }
+    }
+
+    public synchronized void acquireWriteLock() throws InterruptedException {
+        if (isWriting || numReadersReading > 0) {
+            numWritersWaiting++;
+            writerEntering = false;
+            while (!writerEntering) {
+                wait();
+            }
+            numWritersWaiting--;
+        }
+        writerEntering = false;
+        isWriting = true;
     }
 
     public synchronized void releaseWriteLock() {
-        isWriteLocked = false;
-        notify();
+        isWriting = false;
+        writerEntering = (numReadersWaiting == 0);
+        lastReaderAllowedToEnterTimestamp = System.currentTimeMillis();
+        notifyAll();
     }
 }
-
