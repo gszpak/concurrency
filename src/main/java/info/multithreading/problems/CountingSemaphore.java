e0582c0ee1e8fb336186df5412bdef99ce699ad4
Grzegorz Szpak <gszpak@student.uw.edu.pl>
2018-09-29 20:01:06 -0700

    Custom counting semaphore implementation

--- src/main/java/info/multithreading/problems/CountingSemaphore.java
+++ src/main/java/info/multithreading/problems/CountingSemaphore.java
@@ -1,29 +1,40 @@
 package info.multithreading.problems;
 
-class CountingSemaphore {
+public class CountingSemaphore {
 
-    int usedPermits = 0;
-    int maxCount;
+    private int maxCount;
+    private int permitsAvailable;
 
     public CountingSemaphore(int count) {
         this.maxCount = count;
+        this.permitsAvailable = count;
     }
 
     public synchronized void acquire() throws InterruptedException {
-
-        while (usedPermits == maxCount)
+        checkPermitsAvailable();
+        while (permitsAvailable == 0) {
             wait();
-
+        }
+        permitsAvailable--;
+        System.out.flush();
         notify();
-        usedPermits++;
     }
 
     public synchronized void release() throws InterruptedException {
-
-        while (usedPermits == 0)
+        checkPermitsAvailable();
+        while (permitsAvailable == maxCount) {
             wait();
-
-        usedPermits--;
+        }
+        permitsAvailable++;
+        System.out.flush();
         notify();
+    }
+
+    private void checkPermitsAvailable() {
+        if (permitsAvailable < 0 || permitsAvailable > maxCount) {
+            throw new IllegalStateException(
+                    String.format("Permits available: %d out of bounds: [0, %d]",
+                            permitsAvailable, maxCount));
+        }
     }
 }
