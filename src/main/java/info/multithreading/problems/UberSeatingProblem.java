4891b584ef70085a74fdc47611fbeb6e5dcc1f98
Grzegorz Szpak <gszpak@student.uw.edu.pl>
2018-10-06 22:45:43 -0700

    Uber seating problem - custom solution

--- src/main/java/info/multithreading/problems/UberSeatingProblem.java
+++ src/main/java/info/multithreading/problems/UberSeatingProblem.java
@@ -3,90 +3,71 @@ package info.multithreading.problems;
 import java.util.concurrent.BrokenBarrierException;
 import java.util.concurrent.CyclicBarrier;
 import java.util.concurrent.Semaphore;
+import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.ReentrantLock;
 
 public class UberSeatingProblem {
 
-    CyclicBarrier barrier = new CyclicBarrier(4);
-    ReentrantLock lock = new ReentrantLock();
-    private int republicans = 0;
-    private int democrats = 0;
-    private Semaphore demsWaiting = new Semaphore(0);
-    private Semaphore repubsWaiting = new Semaphore(0);
+    private class Party {
+        private int numMembers = 0;
+        private Semaphore semaphore;
 
-    void drive() {
-        System.out.println("Uber Ride on Its wayyyy... with ride leader " + Thread.currentThread().getName());
-        System.out.flush();
+        private Party(Semaphore semaphore) {
+            this.semaphore = semaphore;
+        }
     }
 
+    private CyclicBarrier barrier = new CyclicBarrier(4);
+    private ReentrantLock lock = new ReentrantLock();
+    private Party republicans = new Party(new Semaphore(0));
+    private Party democrats = new Party(new Semaphore(0));
+
     void seatDemocrat() throws InterruptedException, BrokenBarrierException {
-
-        boolean rideLeader = false;
-        lock.lock();
-
-        democrats++;
-
-        if (democrats == 4) {
-            // Seat all the democrats in the Uber ride.
-            demsWaiting.release(3);
-            democrats -= 4;
-            rideLeader = true;
-        } else if (democrats == 2 && republicans >= 2) {
-            // Seat 2 democrats & 2 republicans
-            demsWaiting.release(1);
-            repubsWaiting.release(2);
-            rideLeader = true;
-            democrats -= 2;
-            republicans -= 2;
-        } else {
-            lock.unlock();
-            demsWaiting.acquire();
-        }
-
-        seated();
-        barrier.await();
-
-        if (rideLeader == true) {
-            drive();
-            lock.unlock();
-        }
+        seat(democrats, republicans);
     }
 
     void seatRepublican() throws InterruptedException, BrokenBarrierException {
+        seat(republicans, democrats);
+    }
 
-        boolean rideLeader = false;
+    private void seat(Party party, Party other) throws InterruptedException, BrokenBarrierException {
+        boolean leader = false;
         lock.lock();
-
-        republicans++;
-
-        if (republicans == 4) {
-            // Seat all the republicans in the Uber ride.
-            repubsWaiting.release(3);
-            rideLeader = true;
-            republicans -= 4;
-        } else if (republicans == 2 && democrats >= 2) {
-            // Seat 2 democrats & 2 republicans
-            repubsWaiting.release(1);
-            demsWaiting.release(2);
-            rideLeader = true;
-            republicans -= 2;
-            democrats -= 2;
-        } else {
-            lock.unlock();
-            repubsWaiting.acquire();
-        }
-
-        seated();
+        try {
+            party.numMembers++;
+            if (party.numMembers == 4) {
+                leader = true;
+                party.semaphore.release(3);
+                party.numMembers -= 4;
+            } else if (party.numMembers == 2 && other.numMembers >= 2) {
+                leader = true;
+                party.semaphore.release(1);
+                other.semaphore.release(2);
+                party.numMembers -= 2;
+                other.numMembers -= 2;
+            } else {
+                lock.unlock();
+                party.semaphore.acquire();
+            }
+            seated();
         barrier.await();
-
-        if (rideLeader) {
+        if (leader) {
             drive();
-            lock.unlock();
+        }
+        } finally {
+            if (leader) {
+                lock.unlock();
+            }
         }
     }
 
     void seated() {
-        System.out.println(Thread.currentThread().getName() + "  seated");
+        System.out.println(Thread.currentThread().getName() + " is seated");
+        System.out.flush();
+    }
+
+    void drive() {
+        System.out.println("Driving, leader: " + Thread.currentThread().getName());
         System.out.flush();
     }
 }
